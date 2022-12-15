84a07b8247e53b4a1c099dc4f6f6e42fa1c7c8c7
Marcos Vinicius Vargas Mello <36747665+MarcosMello@users.noreply.github.com>
2022-12-15 03:21:36 +0000

    Pending changes exported from your codespace

--- Arvore Marcos' Version/main.c
+++ Arvore Marcos' Version/main.c
@@ -4,6 +4,35 @@
 #include <stdlib.h>
 
 int main(){
+    Arvore* A = criaArvore();
+
+    printf("\n Inserindo ponto 10\n\n");
+    insereNo(A, criaNo(criaPonto(10,15)), comparaPontos);
+
+    printf("\n Inserindo ponto 15\n\n");
+    insereNo(A, criaNo(criaPonto(15,25)), comparaPontos);
+
+    printf("\n Inserindo ponto 25\n\n");
+    insereNo(A, criaNo(criaPonto(25,5)), comparaPontos);
+
+    printf("\n Inserindo ponto 35\n\n");
+    insereNo(A, criaNo(criaPonto(35,10)), comparaPontos);
+
+    printf("\n Inserindo ponto 5\n\n");
+    insereNo(A, criaNo(criaPonto(5,10)), comparaPontos);
+
+    printf("\n Inserindo ponto 4\n\n");
+    insereNo(A, criaNo(criaPonto(4,10)), comparaPontos);
+
+    printf("\n Inserindo ponto 6\n\n");
+    insereNo(A, criaNo(criaPonto(6,10)), comparaPontos);
+
+    printf("\n Inserindo ponto 16\n\n");
+    insereNo(A, criaNo(criaPonto(16,10)), comparaPontos);
+
+    imprimePonto(A->root->dir->info);
+
+    imprimeDFS(A, imprimePonto);
 
     return 0;
 }
